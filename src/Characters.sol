95ca1b10c5a279f6e48a5dff252310fd225a0e9d
Lyagushka <rayz.sir@gmail.com>
2023-03-10 23:47:11 +0400

    v1.1

--- src/Characters.sol
+++ src/Characters.sol
@@ -1,3 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "lib/openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
+
+abstract contract CharactersParams {
+    uint16 maxSupply;
+}
+
+contract Characters is  CharactersParams, ERC721, ReentrancyGuard {
+
+    constructor(uint16 _newSupply) ERC721("Character", "CHAR") {
+        maxSupply = _newSupply;
+    }
+}
